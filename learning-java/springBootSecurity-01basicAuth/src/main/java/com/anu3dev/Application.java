b0107407d40aaae489f74756250dfc29b9dacd10
anurag git <anurag@git>
2024-12-14 00:04:06 -0600

    adding learning

---
+++ learning-java/springBootSecurity-01basicAuth/src/main/java/com/anu3dev/Application.java
@@ -0,0 +1,15 @@
+package com.anu3dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
+
+/*
+ * Using spring security we can handle authentication and authorization.
+*/
---
+++ learning-java/springBootSecurity-01basicAuth/src/test/java/com/telusko/SecurityProject01/SecurityProject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telusko.SecurityProject01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityProject01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
