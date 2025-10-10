4c3c1c11c71a030623802dae0048860029c468c7
anurag git <anurag@git>
2025-10-09 19:50:00 -0500

    pushing learning

---
+++ ds-practice/One.java
@@ -0,0 +1,8 @@
+public class One {
+    public boolean isPalindrome(){}
+}
+
+One one = new One;
+System.out.println(one.isPalindrome(121))
+System.out.println(one.isPalindrome(-121))
+System.out.println(one.isPalindrome(10))
---
+++ ds-practice/One.js
@@ -0,0 +1,8 @@
+/**
+ * 1. Given an integer x, return true if x is a palindrome, and false otherwise.
+ */
+
+const isPalindrome = () => {}
+console.log(isPalindrome(121))
+console.log(isPalindrome(-121))
+console.log(isPalindrome(10))
