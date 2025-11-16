fdd536ea8ee948d7dffbb4ede1420dcaf9e0676c
anurag git <anurag@git>
2025-11-16 07:17:22 -0600

    adding learning

--- 2025-11-03/07-java/java-002-collection/src/P00Questions.java
+++ 2025-11-03/07-java/java-002-collection/src/P00Questions.java
@@ -6,75 +6,225 @@
 // "Fiona", "HR", 83000
 
 import java.util.ArrayList;
-import java.util.List;
 
+// employee class
 class Employee{
 	String name;
 	String department;
 	double salary;
 	
+	// employee constructor to handle employee object values
 	public Employee(String name, String department, double salary) {
 		this.name = name;
 		this.department = department;
 		this.salary = salary;
 	}
 	
+	// department getter
 	public String getDepartment() {
-		return this.department;
+		return department;
 	}
 	
+	// salary getter
 	public double getSalary() {
-		return this.salary;
+		return salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", department=" + department + ", salary=" + salary + "]";
 	}
 }
 
 class Utils{
-	public void getHighestEarningEmployeeDetails() {
-		// creating employees arraylist
-		List<Employee> employees = new ArrayList<Employee>() ;
-				
-		// inserting value to employees arraylist
+	public ArrayList<Employee> addEmployeeToArrayList() {
+		// creating employees array list
+		ArrayList<Employee> employees = new ArrayList<>();
+		
+		// adding employee details to employee array list as employee object
 		employees.add(new Employee("Alice", "Finance", 92000));
 		employees.add(new Employee("Charlie", "Marketing", 78000));
 		employees.add(new Employee("Bob", "IT", 88000));
 		employees.add(new Employee("Diana", "Finance", 105000));
 		employees.add(new Employee("Evan", "IT", 67000));
 		employees.add(new Employee("Fiona", "HR", 83000));
-				
-		// creating departments arraylist
-		List<String> departments = new ArrayList<String>();
-				
-		// adding unique department value to departments arraylist
-		for(Employee e : employees) {
-			if(!departments.contains(e.getDepartment())) {
-				departments.add(e.getDepartment());
+		
+		return employees;
+	}
+	
+	public ArrayList<String> getArrayListWithUniqueDepartmentValue(){
+		ArrayList<Employee> employeeList = addEmployeeToArrayList();
+		
+		// creating departments arraylist with unique values
+		ArrayList<String> departments = new ArrayList<>();
+		
+		for(Employee emp : employeeList) {
+			if(!departments.contains(emp.getDepartment())) {
+				departments.add(emp.getDepartment());
 			}
 		}
+		
+		return departments;
+	}
+	
+	public Employee getHighestEarningEmployeeDetailsAmongDepartments() {
+		ArrayList<Employee> employeeList = addEmployeeToArrayList();
+		
+		// getting employee object with high salary
+		Employee employeeWithHighestSalary = null;
+		
+		for(Employee emp : employeeList) {
+			if(employeeWithHighestSalary == null ||
+					employeeWithHighestSalary.getSalary() < emp.getSalary()) {
+				employeeWithHighestSalary = emp;
+			}
+		}
+		
+		return employeeWithHighestSalary;
+	}
+	
+	public ArrayList<Employee> getHighestEarningEmployeeDetailsFromEachDepartment(){
+		ArrayList<String> departmentList = getArrayListWithUniqueDepartmentValue();
+		ArrayList<Employee> employeeList = addEmployeeToArrayList();
+		
+		ArrayList<Employee> finalFilteredEmployeeArrayList = new ArrayList<>();
+		
+		Employee highestEarningEmployeeDetailsFromEachDepartment = null;
 				
-		// Initializing a Employee list with null
-		Employee highestIncomeEmployee = null;
-				
-		// fetching list with highest income
-		for(String d : departments) {
-			for(Employee e : employees) {
-				if(e.getDepartment().equals(d)) {
-					if(highestIncomeEmployee == null 
-							|| highestIncomeEmployee.getSalary() < e.getSalary()) {
-						highestIncomeEmployee = e;
+		for(String dept : departmentList) {
+			for(Employee emp : employeeList) {
+				if(emp.getDepartment().equals(dept)) {
+					if(highestEarningEmployeeDetailsFromEachDepartment == null || 
+							highestEarningEmployeeDetailsFromEachDepartment.getSalary() < emp.getSalary() || 
+									!highestEarningEmployeeDetailsFromEachDepartment.getDepartment().equals(dept)) {
+						highestEarningEmployeeDetailsFromEachDepartment = emp;
 					}
 				}
 			}
+			finalFilteredEmployeeArrayList.add(highestEarningEmployeeDetailsFromEachDepartment);
 		}
-				
-		// printing result
-		System.out.println("highest salary is : " + highestIncomeEmployee.getSalary()
-		+ " department name is : " + highestIncomeEmployee.getDepartment());
+		
+		return finalFilteredEmployeeArrayList;
+	}
+	
+	public ArrayList<Employee> getHighestEarningEmployeeDetailsFromEachDepartmentInSortedOrder(){		
+		// selection sort example
+		ArrayList<Employee> highestSalaryFromEachDept1 = getHighestEarningEmployeeDetailsFromEachDepartment();
+		
+		for(int i = 0; i < highestSalaryFromEachDept1.size(); i++) {
+			for(int j = i + 1; j < highestSalaryFromEachDept1.size(); j++) {
+				if(highestSalaryFromEachDept1.get(j).getSalary() < highestSalaryFromEachDept1.get(i).getSalary()) {
+					Employee temp = highestSalaryFromEachDept1.get(j);
+					highestSalaryFromEachDept1.set(j, highestSalaryFromEachDept1.get(i));
+					highestSalaryFromEachDept1.set(i, temp);
+				}
+			}
+		}
+		
+		// bubble sort example
+		ArrayList<Employee> highestSalaryFromEachDept2 = getHighestEarningEmployeeDetailsFromEachDepartment();
+		
+		for(int i = 0; i < highestSalaryFromEachDept2.size() - 1; i++) {
+			for(int j = 0; j < highestSalaryFromEachDept2.size() - 1 - i; j++) {
+				if(highestSalaryFromEachDept2.get(j + 1).getSalary() < highestSalaryFromEachDept2.get(j).getSalary()) {
+					Employee temp = highestSalaryFromEachDept2.get(j + 1);
+					highestSalaryFromEachDept2.set(j + 1, highestSalaryFromEachDept2.get(j));
+					highestSalaryFromEachDept2.set(j, temp);
+				}
+			}
+		}
+		
+		// quick sort example
+		ArrayList<Employee> highestSalaryFromEachDept3 = getHighestEarningEmployeeDetailsFromEachDepartment();
+		
+		return quickSorting(highestSalaryFromEachDept3);
+	}
+	
+	public ArrayList<Employee> quickSorting(ArrayList<Employee> emp){
+		
+	    if (emp.size() < 2) {
+	        return emp;
+	    }
+		
+		Employee pivot = emp.get(emp.size() -1);
+		ArrayList<Employee> left = new ArrayList<Employee>();
+		ArrayList<Employee> right = new ArrayList<Employee>();
+		
+		for(int i = 0; i < emp.size() - 1; i++) {
+			if(emp.get(i).getSalary() < pivot.getSalary()) {
+				left.add(emp.get(i));
+			} else {
+				right.add(emp.get(i));
+			}
+		}
+		
+		ArrayList<Employee> sortedArrayList = new ArrayList<Employee>();
+		
+		sortedArrayList.addAll(quickSorting(left));
+		sortedArrayList.add(pivot);
+		sortedArrayList.addAll(quickSorting(right));
+		
+		return sortedArrayList;
+	}
+	
+	public String getReverse(String str) {
+		String rev = "";
+		
+		for(int i = str.length() - 1; i >= 0; i--) {
+			rev += str.charAt(i);
+		};
+		return rev;
+	}
+	
+	public boolean isPalindrome(String str) {
+		for(int i = 0; i < str.length()/2; i++) {
+			if(str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+				return false;
+			}
+		};
+		return true;
+	}
+	
+	public int getReverse(int num) {
+		if (num < 0) return 0;
+		
+		int temp = num;
+		int rev = 0;
+
+		while(temp != 0) {
+			rev = rev * 10 + temp % 10;
+			temp = temp / 10;
+		}
+		
+		return rev;
+	}
+	
+	public boolean isPalindrome(int num) {
+		
+		return num == getReverse(num);
 	}
 }
+
 public class P00Questions{
 	public static void main(String[] args) {
-		// get highest earning employee
 		Utils utils = new Utils();
-		utils.getHighestEarningEmployeeDetails();
+		
+		System.out.println(utils.getHighestEarningEmployeeDetailsAmongDepartments());
+		
+		System.out.println(utils.getHighestEarningEmployeeDetailsFromEachDepartment());
+		
+		System.out.println(utils.getHighestEarningEmployeeDetailsFromEachDepartmentInSortedOrder());
+		
+		System.out.println("reverse is : " + utils.getReverse("anurag"));
+		
+		System.out.println("reverse is : " + utils.getReverse(9876));
+		
+		System.out.println("is palindrome : " + utils.isPalindrome("madam"));
+		
+		System.out.println("is palindrome : " + utils.isPalindrome("listen"));
+		
+		System.out.println("is palindrome : " + utils.isPalindrome(12321));
+		
+		System.out.println("is palindrome : " + utils.isPalindrome(12345));
 	}
 }
--- ds-practice/One.java
+++
@@ -1,8 +0,0 @@
-public class One {
-    public boolean isPalindrome(){}
-}
-
-One one = new One;
-System.out.println(one.isPalindrome(121))
-System.out.println(one.isPalindrome(-121))
-System.out.println(one.isPalindrome(10))
