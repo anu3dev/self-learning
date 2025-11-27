3af9f747cc46561ecccf59b9ffa52cd7a25e0bb5
anurag git <anurag@git>
2025-11-26 19:26:18 -0600

    adding learning

--- 2025-11-03/00-practice/0005-controlled-and-uncontrolled-component/src/App.tsx
+++ 2025-11-03/00-practice/0005-controlled-and-uncontrolled-component/src/App.tsx
@@ -13,3 +13,42 @@ function App() {
 }
 
 export default App
+
+/**
+ * React.PureComponent automatically implements shouldComponentUpdate() 
+ * with a shallow comparison of props and state.
+ * 
+ * It re-renders only if props or state actually change.
+ * Prevents unnecessary re-renders.
+ * 
+ * class MyComponent extends React.PureComponent {
+    render() {
+      return <div>{this.props.value}</div>;
+    }
+  }
+ *
+ */
+
+
+/**
+ * React Lifecycle = 3 phases:
+ *    Mounting (component born)
+ *    Updating (component changes)
+ *    Unmounting (component removed)
+ * 
+ * Mounting Methods
+ *    constructor → initialize state
+ *    getDerivedStateFromProps → sync props → state
+ *    render → return JSX
+ *    componentDidMount → API calls, DOM access
+ * 
+ * Updating Methods
+ *    getDerivedStateFromProps → sync props
+ *    shouldComponentUpdate → decide re-render
+ *    render → redraw
+ *    getSnapshotBeforeUpdate → read DOM before update
+ *    componentDidUpdate → logic after update
+ * 
+ * Unmounting Method
+ *    componentWillUnmount → cleanup (listeners, timers, API cancels)
+ */
